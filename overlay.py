944aef1a581e166c72c09a89b679daf8d9661a92
davidbrown2324 <david.brown3@ucsf.edu>
2020-03-25 20:53:24 -0700

    try to make overlay.py more generic

--- overlay.py
+++ overlay.py
@@ -3,16 +3,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 ##Make a generic plot function.
-def overlayPlot(landing_pad_df: pd.DataFrame, x_channel: str, y_channel: str, **kwargs):
+def overlayPlot(data1: pd.DataFrame, data2: pd.DataFrame, x_channel: str, y_channel: str, **kwargs):
     '''function to generate a plot comparing mean signal across a number of bins'''
     
     #Get **kwargs
+    bins      = kwargs.get('bins', None)
     plot      = kwargs.get('plot', True)
     title     = kwargs.get('title', 'overlayPlot_figure.pdf')
     con_color = kwargs.get('con_color', 'lightgrey') 
     exp_color = kwargs.get('exp_color', 'green')
     x_label   = kwargs.get('x_label', x_channel)
     y_label   = kwargs.get('y_label', y_channel)
+    labels    = kwargs.get('xlim', ['control', 'experiment'])
+    xlim      = kwargs.get('xlim', [-1000, 10000])
+    ylim      = kwargs.get('ylim', [-1000, 10000])
     save      = kwargs.get('save', False)
     
     fig, ax = plt.subplots()
@@ -20,25 +24,25 @@ def overlayPlot(landing_pad_df: pd.DataFrame, x_channel: str, y_channel: str, **
     if plot==False:
         plt.ioff()
 
-    control_data = landing_pad_df[landing_pad_df.condition.eq('control')]
-    control_data.plot(ax=ax, x=x_channel, y=y_channel, kind='scatter', c='lightgrey', alpha =0.1)
-
-    experiment_data = landing_pad_df[landing_pad_df.condition.eq('experiment')]
-    experiment_data.plot(ax=ax, x=x_channel, y=y_channel, kind='scatter', c='green', alpha =0.1)
-
-    colors = {'control':'grey', 'experiment':'green'}
+    data1.plot(ax=ax, x=x_channel, y=y_channel, kind='scatter', c=con_color, alpha =0.1)
+    data2.plot(ax=ax, x=x_channel, y=y_channel, kind='scatter', c=exp_color, alpha =0.1)
 
     #Bin data into 22 bins
-    landing_pad_df.loc[:,'bin'] = pd.cut(landing_pad_df[x_channel], list(range(-1000, 10000, 500)))
+    if bins is None:
+        bins = list(range(-1000, 10000, 500))
+        
+    data1.loc[:,'bin'] = pd.cut(data1[x_channel], bins)
+    data2.loc[:,'bin'] = pd.cut(data2[x_channel], bins)
 
     #Group by bin
-    landing_pad_df = landing_pad_df.groupby(by=['landing_pad', 'condition','bin']).mean().reset_index()
-    landing_pad_df
+    data1 = data1.groupby(by=['bin']).mean().reset_index()
+    data2 = data2.groupby(by=['bin']).mean().reset_index()
 
-    grouped = landing_pad_df.groupby(by='condition')
-    for key, group in grouped:
-        group.plot(ax=ax, kind='line', x=x_channel, y=y_channel, label=key, color=colors[key])
-
+    #for n, data in enumerate([data1, data2]):
+    #    data.plot(ax=ax, kind='line', x=x_channel, y=y_channel, color=colors[n])
+    data1.plot(ax=ax, kind='line', x=x_channel, y=y_channel, label=labels[0], color=con_color, xlim=xlim, ylim=ylim)
+    data2.plot(ax=ax, kind='line', x=x_channel, y=y_channel, label=labels[1], color=exp_color, xlim=xlim, ylim=ylim)
+        
     #Label Plot
     plt.xlabel(x_label);
     plt.ylabel(y_label);
