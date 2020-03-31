bc7eecbde22df1078a58f86d4ed3dea42cfed8f2
davidbrown2324 <david.brown3@ucsf.edu>
2020-03-31 14:50:47 -0700

    automate the early steps of processing for FACS data

---
+++ experiment.py
@@ -0,0 +1,26 @@
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from alternativeFACS.alternativeFACS.helpers.saturation  import *
+from alternativeFACS.alternativeFACS.helpers.contours import *
+from alternativeFACS.alternativeFACS.helpers.singlets import *
+
+def processExperiment(experiment: pd.DataFrame, limit_dict: dict, polygon: plt.Polygon, singlet_threshold: float):
+    '''remove saturation, gate event scattering based on a supplied polygon and eliminate doublet events'''
+    
+    #[1] Remove saturation
+    mask = maskSaturation(experiment, limit_dict)
+    unsaturated = mask.dropna()
+    
+    #[2] Scatter gate
+    scatterGate(unsaturated, polygon)
+    
+    #[3] Singlet gate
+    singlets = singletGate(unsaturated, singlet_threshold)
+    
+    #[4] Limit to singlets
+    singlets = singlets[singlets['Scatter+'] & singlets['Singlet+']]
+    
+    return singlets
