a69c90c0e3b5b1edf70a27f253a8d6ed62dbb06e
davidbrown2324 <david.brown3@ucsf.edu>
2020-04-07 09:30:01 -0700

    fix issue #26 by eliminating requirment for limit_dict

--- saturation.py
+++ saturation.py
@@ -9,25 +9,33 @@ def maskChannelSaturation(df, channel, lower, upper):
     return df[channel].mask(~df[channel].between(lower, upper, inclusive = False))
 
 #Iterate through channels
-def maskSaturation(df: pd.DataFrame, limit_dict: dict, verbose=False):
+def maskSaturation(df: pd.DataFrame, **kwargs):
     '''replace values outside channel limits with NaN'''
+    
+    #Get **kwargs
+    verbose    = kwargs.get('verbose', False)
+    limit_dict = kwargs.get('limit_dict', {})
+    FACS_channels = kwargs.get('FACS_channels', ['FSC-A', 'FSC-H', 'FSC-W', 'SSC-A'])
+    fluorescence_channels = kwargs.get('fluorescence_channels', ['405nm', '488nm', '564nm', '646nm'])
     
     if verbose:
         print('Input events =', len(df))
 
-    #You have to use pd.DataFrame.copy() otherwise you will generate a view which will get overwritten
+    #Use pd.DataFrame.copy() otherwise you will generate a view which will get overwritten
     mask = df.copy()
 
     for channel in df.columns:
-        if channel in limit_dict.keys():
+        if channel in FACS_channels + fluorescence_channels:
         
             if verbose:
                 print('Removing',channel, 'saturation')
 
-            #Retrive channel specific limits from dictionary
-            lower = limit_dict[channel]['lower_limit']
-            upper = limit_dict[channel]['upper_limit']
-
+            #Retrive channel specific limits from dictionary, default to 0, 262144 = 18-bit range
+            limits = limit_dict.get(channel, {'lower_limit': 0, 'upper_limit': 262144})
+            
+            lower = limits['lower_limit']
+            upper = limits['upper_limit']
+            
             #Mask events not between the upper and lower limits
             mask[channel] = maskChannelSaturation(mask, channel, lower, upper)
     
