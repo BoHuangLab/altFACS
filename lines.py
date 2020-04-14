821e2b8847966eaf396b04e92c2bb49e5c89eb0f
davidbrown2324 <david.brown3@ucsf.edu>
2020-04-13 20:26:48 -0700

    fitGated function, brings this paper in line with split-sfCherry3 analysis

--- lines.py
+++ lines.py
@@ -71,3 +71,39 @@ def rescalePlot(data, x_channel:str, y_channel:str, slope:float, intercept:float
 
 def piecewise_linear(x, x0, y0, k1, k2):
     return np.piecewise(x, [x < x0], [lambda x:k1*x + y0-k1*x0, lambda x:k2*x + y0-k2*x0])
+
+def fitGated(data, x_channel, y_channel):
+    '''this function allows y_gated data to be fitted accurately
+    
+    Along the lines of the strategy employed in our split-sfCherry3 paper.
+    https://static-content.springer.com/esm/art%3A10.1038%2Fs42003-019-0589-x/MediaObjects/42003_2019_589_MOESM1_ESM.pdf
+    '''
+    
+    x=data[x_channel]
+    y=data[y_channel]
+
+    plt.scatter(x, y, s=2, alpha=0.3, c='green')
+    plt.xlim(1,5);
+    plt.ylim(1,5);
+    plt.yticks([1,2,3,4,5]);
+    plt.xlabel(x_channel);
+    plt.ylabel(y_channel); 
+
+    #Set the axis to be equal
+    plt.gca().set_aspect('equal');
+
+    #Fit y against x
+    slope, intercept, r_value, p_value, std_err = stats.linregress(y, x)
+    #In this case both slope and intercept are fitted parameters.
+    
+    gradient = 1/slope
+    offset   = -intercept/slope
+    
+    print("slope:%f intercept:%f p_value: %f std_err:%f " % (gradient, offset, p_value, std_err))
+
+    xx = pd.Series([x.min(), x.max()])
+
+    yy = xx.apply(lambda xx: gradient*xx+offset)
+
+    plt.plot(xx, yy, c='red');
+    plt.title(file_info.loc[file_num, 'Description']);
