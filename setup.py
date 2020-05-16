c5187b15a7657eb3945407cc2316af50eb144a0f
davidbrown2324 <david.brown3@ucsf.edu>
2020-05-15 21:01:23 -0700

    add __init__.py and setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="altFACS",                          #100% required
+    version="1.0.0",                         #100% required
+    description="FACS tools for split fluorescent proteins",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/davidbrown2324/altFACS",
+    author="David Brown",
+    author_email="david.brown3@ucsf.edu",
+    license="MIT",                                  
+    classifiers=[
+        #"License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["altFACS"],                    #100% required
+    include_package_data=True,
+#    install_requires=["feedparser", "html2text"],   #what should these be?
+#    entry_points={
+#        "console_scripts": [
+#            "alternativeFACS=alternativeFACS.alternativeFACS:main",      #what should these be?
+#        ]
+#    },
+)
