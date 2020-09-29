08e823682dfb5426ded01cefa4b79c03449b2c56
davidbrown2324 <david.brown3@ucsf.edu>
2020-09-29 18:30:24 +0100

    started adding docstrings

--- altFACS/fluorescence.py
+++ altFACS/fluorescence.py
@@ -2,22 +2,23 @@ import sys
 import numpy as np
 import pandas as pd
 
-def transformFluorescenceChannels(data: pd.DataFrame, channels: list, transform, inplace=False)->pd.DataFrame:
+def transformFluorescenceChannels(df: pd.DataFrame, channels: list, transform, inplace=False)->pd.DataFrame:
     '''Transform values in list'''
     
     if inplace:
         for channel in channels:
-            data.loc[:,channel] = transform(data.loc[:,channel])
-        return data
+            df.loc[:,channel] = transform(df.loc[:,channel])
+        return df
     
     else:
         output = data.copy()
 
         for channel in channels:
-            output.loc[:,channel] = transform(data.loc[:,channel])
+            output.loc[:,channel] = transform(df.loc[:,channel])
         return output
 
-def autothresholdChannel(data, channel, **kwargs)->float:
+    
+def autothresholdChannel(df: pd.DataFrame, channel: str, **kwargs)->float:
     '''
     Determine a threshold of a channel by mean + n_stdevs * std or by a set percentile.
     '''
@@ -28,17 +29,39 @@ def autothresholdChannel(data, channel, **kwargs)->float:
     
     if n_stdevs is not None:
         # mean + n * standard deviation method
-        threshold = data[channel].mean() + n_stdevs*data[channel].std()
+        threshold = df[channel].mean() + n_stdevs*df[channel].std()
     
     else:
         # percentile method
-        threshold = data[channel].quantile(percentile)
+        threshold = df[channel].quantile(percentile)
     
     return threshold
 
-def autothreshold(data, channels,  **kwargs)->dict:
+
+def autothreshold(df: pd.DataFrame, channels: list,  **kwargs)->dict:
     '''
     threshold each channel in the list
+    
+    Parameters:
+    df: pd.DataFrame
+    data containing the columns for thresholding 
+    
+    channels: list
+    A list containing the column names for thresholding 
+    
+    
+    Optional Key Word Argument Parameters:
+    percentile: float
+    the percentile at which to set the threshold. This value will not be used if n_stdevs is given.
+    
+    n_stdevs: float
+    the number of standard deviations above the mean to set the threshold.
+    
+    
+    Returns:
+    thresholds_dict: dict
+    a dictionary containing the threshold for each channel in channels.
+    
     '''
     
     #Get **kwargs
@@ -51,16 +74,16 @@ def autothreshold(data, channels,  **kwargs)->dict:
         
         if n_stdevs is not None:
             # mean + n * standard deviation method
-            thresholds_dict[channel] = autothresholdChannel(data, channel, n_stdevs=n_stdevs)
+            thresholds_dict[channel] = autothresholdChannel(df, channel, n_stdevs=n_stdevs)
             
         else:
             # percentile method
-            thresholds_dict[channel] = autothresholdChannel(data, channel, percentile=percentile)
-
+            thresholds_dict[channel] = autothresholdChannel(df, channel, percentile=percentile)
             
     return thresholds_dict
-    
-def channelGate(data, channels, thresholds_dict):
+
+
+def channelGate(df: pd.DataFrame, channels: list, thresholds_dict: dict):
     '''Add boolean gates for each channel in the list
     based on a dictionary of thresholds.
     '''
@@ -69,7 +92,7 @@ def channelGate(data, channels, thresholds_dict):
         gate_name = channel+"+"
         
         #Add boolean gates
-        data.loc[:, gate_name] = data[channel] > thresholds_dict[channel]
+        df.loc[:, gate_name] = df[channel] > thresholds_dict[channel]
         #Should these modify the input or just return the booleans?
        
-    return data
+    return df
