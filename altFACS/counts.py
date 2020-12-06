cac460b9de7a6bac6e5d2cde9f1c412760e8b7b4
davidbrown2324 <david.brown3@ucsf.edu>
2020-12-06 12:59:42 +0000

    add docstrings to counts.py

--- altFACS/counts.py
+++ altFACS/counts.py
@@ -3,22 +3,41 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def combineGates(data: pd.DataFrame, gate1: str, gate2: str):
-  
-    assert gate1 in data.columns
-    assert gate2 in data.columns
+def combineGates(df: pd.DataFrame, gate1: str, gate1: str):
+    '''
+    Combine boolean 'gate' columns on annotated dataframe.
     
-    #If there is no + at the end of the in put string
+    Parameters:
+    df: pd.DataFrame
+    FACS data.
+    
+    gate1: str
+    Name of gate1 column. Assumes a wavelength followed by a '+', e.g. 405nm+
+    
+    gate2: str
+    Name of gate2 column. Assumes a wavelength followed by a '+', e.g. 488nm+
+    
+    
+    Returns:
+    df: pd.DataFrame
+    FACS data with new columns for the combined gates. e.g '405nm_neg_488nm_neg'.
+    
+    '''
+  
+    assert gate1 in df.columns
+    assert gate2 in df.columns
+    
+    #If there is no + at the end of the input string
     if gate1[-1] != '+':
         gate1 = gate1 + '+'
     
     if gate2[-1] != '+':
         gate2 = gate2 + '+'
            
-    double_neg = ~data[gate1] & ~data[gate2]
-    c1_pos     =  data[gate1] & ~data[gate2]
-    c2_pos     = ~data[gate1] &  data[gate2]
-    double_pos =  data[gate1] &  data[gate2]
+    double_neg = ~df[gate1] & ~df[gate2]
+    c1_pos     =  df[gate1] & ~df[gate2]
+    c2_pos     = ~df[gate1] &  df[gate2]
+    double_pos =  df[gate1] &  df[gate2]
     
     #handle gate or channel inputs
     channel1=gate1.replace('+', '', 1)
@@ -29,15 +48,35 @@ def combineGates(data: pd.DataFrame, gate1: str, gate2: str):
     n_p = channel1+"_neg_"+channel2+"_pos"
     p_p = channel1+"_pos_"+channel2+"_pos"
     
-    data.loc[:, n_n] = double_neg
-    data.loc[:, p_n] = c1_pos
-    data.loc[:, n_p] = c2_pos
-    data.loc[:, p_p] = double_pos
+    df.loc[:, n_n] = double_neg
+    df.loc[:, p_n] = c1_pos
+    df.loc[:, n_p] = c2_pos
+    df.loc[:, p_p] = double_pos
     
-    return data
+    return df
 
 
 def hitRate(df, double_neg_col, c1_pos_col, c2_pos_col, double_pos_col):
+    '''
+    Calculate the hit rate (%); double_pos / (double_pos + c1_pos) *100, 
+    and the false detection rate; c2_pos / (double_neg + c2_pos) *100.
+    
+    Parameters:
+    df: pd.DataFrame
+    FACS data.
+    
+    double_neg_col, c1_pos_col, c2_pos_col, double_pos_col:str
+    The names of columns in df containing the combined gating information. c1 is channel1, c2 is channel2.
+    
+    
+    Returns:
+    HR:
+    Hit Rate.
+    
+    FDR:
+    False Detection Rate.
+    
+    '''
 
     #Calculate Hit Rate 
     HR = (df[double_pos_col]/(df[double_pos_col]+df[c1_pos_col]))*100
@@ -51,8 +90,34 @@ def hitRate(df, double_neg_col, c1_pos_col, c2_pos_col, double_pos_col):
 
     return HR, FDR
 
+
 def countPlot(counts: pd.DataFrame, stat='HR', **kwargs) -> plt.Figure:
-    '''function to plot (and save) bar graphs comparing experiment to controls'''
+    '''
+    Plot (and save) bar graphs comparing experiment to controls
+    
+    Parameters:
+    counts: pd.DataFrame
+    A dataframe with stats in different columns e.g. HR, or FDR.
+    
+    stat: str
+    
+    
+    Optional Parameters:
+    plot:
+    
+    title:
+    
+    width:
+    
+    con_color:
+    
+    exp_color:
+    
+    legend:
+    
+    save:
+    
+    '''
     
     #Expects a dataframe with stats in different columns e.g. HR, or FDR
     
