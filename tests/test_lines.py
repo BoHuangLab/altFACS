79ca03723c4913f050026f531c51d5c9313c6534
davidbrown2324 <david.brown3@ucsf.edu>
2020-12-07 22:21:12 +0000

    add a WIP test for lines.py

---
+++ tests/test_lines.py
@@ -0,0 +1,15 @@
+import unittest
+
+import numpy as np
+# import pandas as pd
+
+from altFACS.lines  import *
+
+
+# class TestSinglets(unittest.TestCase):
+    
+#     def test_rescale(self):
+        
+
+if __name__ == "__main__":
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
